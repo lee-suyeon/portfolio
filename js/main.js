6d270dfe2d9b738c1ade4bd5dbf1770a8889ffcb
sroovy <42996064+sroovy@users.noreply.github.com>
2020-03-28 13:10:53 +0900

    20200327

--- js/main.js
+++ js/main.js
@@ -1,15 +1,84 @@
 $(function () {
-    $(".ham").click(function (e) {
+
+    
+    $("html,body").stop().animate({scrollTop: 0});
+
+    // home
+    $(".home").click(function (e) {
         e.preventDefault();
-        $(this).toggleClass("active");
-        $(".overlay").toggle();
+        $("html,body").stop().animate({scrollTop: 0});
     });
 
+    
+    // menu button 
+    var $menu = $(".nav .gnb");
+    var $hamButton = $(".ham");
 
-   $(".photo").addClass("show");
+    $hamButton.click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass("active");
+        $menu.toggleClass("open");
+    });
 
-   $(".photo .chatbubble").click(function () {
+    // gnb 
+    var $navButton = $(".gnb ul li");
+    var $contents = $("section");
+    
+    $navButton.click(function (e) {
+        e.preventDefault();
+
+        var idx = $(this).index();
+        var section = $contents.eq(idx);
+        var sectionDistance = section.offset().top;
+
+        $("html,body").stop().animate({scrollTop:sectionDistance}, 200);
+        $menu.removeClass("open");
+        $hamButton.removeClass("active");
+    });
+
+    // photo
+    var $photo = $(".photo");
+    $photo.addClass("show");
+
+    $photo.find(".chatbubble").click(function () {
         $(".dots").hide();
         $(this).find(".welcome").show();
+   });
+
+   //scroll button
+   var profilePos = $("#about").offset().top;
+
+   $(".scroll_button").click(function (e) {
+       e.preventDefault();
+        $("html,body").stop().animate({scrollTop:profilePos});
    })
+   
+   // wheel event
+
+
+   // ---------------------about--------------------
+  
+
+
+   // skill button
+   $(".skillbutton").one("click", function () {
+       $(this).addClass("active").css({animation:"none"});
+
+       $(".skillbox").each(function () {
+            var progressRate = $(this).attr("data-rate"),
+                progressText = $(this).find(".rate");
+
+            $(this).find(".bar").animate({width: progressRate + "%"},1000);
+
+            $({rate:0}).animate({rate:progressRate},{
+                duration: 1000,
+                progress: function () {
+                    var now = this.rate;
+                    progressText.text(Math.ceil(now) + "%");
+                }
+            });
+        });
+    }); //click event
+
+
 }); 
